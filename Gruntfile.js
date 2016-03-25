9fc1d11201ca332f4bb3359e175d18e8f0ff3f04
Arjun Curat <nagarjun.curat@gmail.com>
2016-03-25 18:07:10 -0500

    Bug fix

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg : grunt.file.readJSON('package.json'),
+
+		clean : {
+			js : [ "js/main*.js" ]
+		}
+		uglify : {
+			build : {
+				src : [ 'js/main.js' ],
+				dest : 'js/main.min.js'
+			},
+			options : {
+				mangle : true
+			}
+		},
+
+		jshint : {
+			src : [ 'Gruntfile.js', 'js/main.js' ]
+
+		},
+
+		cssmin : {
+			target : {
+				files : [ {
+					expand : true,
+					src : 'css/*.css',
+					ext : '.min.css'
+				} ]
+			}
+		}
+	});
+
+	// Load the plugin that provides the "uglify" task.
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+	// Default task(s).
+	grunt.registerTask('default', [ 'cssmin', 'clean', 'jshint','uglify', 'cssmin' ]);
+
+};
